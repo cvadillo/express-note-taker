6fd7f5ed0a4fe0242f4723cdc44c0a8bc6725d5d
Carlos V <cavarod@gmail.com>
2021-04-16 10:20:24 -0700

    trying to get heroku to run

--- server.js
+++ server.js
@@ -11,11 +11,21 @@ const PORT = process.env.PORT || 3001;
 // Initialize epxress as a variable
 const app = express();
 
+// API endpoint
+app.get('/api/db', (req, res) => {
+  res.json(notes);
+});
+
 // Initialize a static folder 'Public'
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.get('/api/db', (req, res) => {
-  res.json(notes);
+// Add two routes for the two pages
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
+app.get('/notes', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'notes.html'));
 });
 
 // Initialize the server listening
