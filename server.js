86e8cb1f51e1b2f53e6a446ee84ed498f093020d
Carlos V <cavarod@gmail.com>
2021-04-16 10:15:26 -0700

    aded heroku

--- server.js
+++ server.js
@@ -1,14 +1,22 @@
+// Required Packages
 const express = require('express');
 const path = require('path');
+
+// API routes
+const { notes } = require('./db/db');
 
 // Initialize epxress as a variable
 const app = express();
 
+// Assign a port for the local server to listen to
+const PORT = process.env.PORT || 3001;
+
 // Initialize a static folder 'Public'
 app.use(express.static(path.join(__dirname, 'public')));
 
-// Assign a port for the local server to listen to
-const PORT = process.env.PORT || 3001;
+app.get('/api/db', (req, res) => {
+  res.json(notes);
+});
 
 // Initialize the server listening
 app.listen(PORT, () => console.log(`The server has been initiated on port ${PORT}`));
