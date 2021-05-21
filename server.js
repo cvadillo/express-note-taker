478246c5be2710831efa13d728ffc7d189afc8c4
Carlos V <cavarod@gmail.com>
2021-05-21 08:42:21 -0700

    took another approach with the APIs after completing the last project

--- server.js
+++ server.js
@@ -1,34 +1,16 @@
-// Required Packages
 const express = require('express');
-const path = require('path');
-
-// API routes
-const { notes } = require('./db/db');
-
-// Assign a port for the local server to listen to
-const PORT = process.env.PORT || 3001;
-
-// Initialize epxress as a variable
 const app = express();
+const PORT = process.env.PORT || 3001;
+const htmlRoutes = require('./routes/htmlRoutes');
+const apiRoutes = require('./routes/apiRoutes');
 
-// API endpoint
-app.get('/api/db', (req, res) => {
-  res.json(notes);
-});
+app.use(express.urlencoded({extended: true}));
+app.use(express.static('public'));
+app.use(express.json());
+app.use('/api', apiRoutes);
+app.use('/', htmlRoutes);
 
-// Initialize a static folder 'Public'
-app.use(express.static(path.join(__dirname, 'public')));
 
-// Add two routes for the two pages
-app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public', 'index.html'));
-});
-
-app.get('/notes', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public', 'notes.html'));
-});
-
-// Initialize the server listening
 app.listen(PORT, () => {
-	console.log(`The server has been initiated on port ${PORT}`)
+    console.log(`API server now on port ${PORT}`);
 });
